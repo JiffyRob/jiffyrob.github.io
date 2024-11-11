d1622e29fbe03bf4f1ca26b066ec753e5a181dcb
JiffyRob <jfrobinson767@gmail.com>
2024-11-10 23:40:54 -0600

    added social media buttons

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
+// Button API
+const github = function() {
+    window.open("https://github.com/jiffyrob", "_blank");
+}
+
+const itchio = function() {
+    window.open("https://itch.io/profile/jiffyrob", "_blank");
+}
+
+const soundcloud = function() {
+    window.open("https://soundcloud.com/jiffyrob/albums", "_blank");
+}
+
+const rickroll = function() {
+    window.open("https://youtu.be/E4WlUXrJgy4", "_blank");
+}
+
+const discord = function() {
+    window.open("discord.html", "popup", 'height=200,width=300')
+};
+
 // Set background image to be random from selection of background images
 const backgroundImages = ["mushrooms", "exploding-cows"]
 const backgroundImage = backgroundImages[Math.floor(Math.random() * backgroundImages.length)];
